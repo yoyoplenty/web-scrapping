7b464b367cd781bc731c3db7b7dcd12acd61b1ca
Adebiyi <yoyoplenty@gmail.com>
2022-07-13 13:35:08 +0100

    update

--- controllers/data.js
+++ controllers/data.js
@@ -1,32 +1,7 @@
 const AppError = require("../utils/appError");
-const fetch = require("node-fetch");
 const puppeteer = require("puppeteer");
-
-// /* (async () => {
-//     const browser = await puppeteer.launch({ headless: false });
-//     page = await browser.newPage();
-//     await page.goto("https://www.jumia.com.ng/phones-tablets/");
-
-//     /* Run javascript inside the page */
-//     const data = await page.evaluate(() => {
-//         const list = [];
-//         const items = document.querySelectorAll("div.itm");
-
-//         for (const item of items) {
-//             list.push({
-//                 name: item.querySelector(".name").innerHTML,
-//                 price: item.querySelector(".prc").innerHTML,
-//                 // link: "https://jumia.com.ng" + item.getAttribute("data-href"),
-//             });
-//         }
-
-//         return list;
-//     });
-//     console.log(data);
-//     await browser.close();
-// })();
-
 const url = "https://www.jumia.com.ng/phones-tablets/";
+const ScrappedData = require("../models/data");
 
 async function configureTheBrowser() {
     const browser = await puppeteer.launch();
@@ -39,27 +14,25 @@ async function checkDetails(page) {
     let data = await page.evaluate(() => {
         const list = [];
         const items = document.querySelectorAll("div.itm");
-
         for (const item of items) {
+            console.log(item);
             list.push({
-                //  name: item.querySelector(".name").innerHTML,
-                //                 price: item.querySelector(".prc").innerHTML,
-                link: item.querySelector("a.core").getAttribute("data-src"),
-                //img: link.getAttribute("data-src"),
-                img1: item.getAttribute("data-src"),
-                // img2: link.getAttribute("data-src"),
-                // link: "https://jumia.com.ng" + item.getAttribute("data-href"),
+                name: item.querySelector(".name").innerHTML,
+                price: item.querySelector(".prc").innerHTML,
+                link: item.querySelector(".core").href,
+                img_url: item.querySelector(".img").getAttribute("data-src"),
             });
         }
         return list;
     });
-    console.log(data);
+    return data;
 }
 
 exports.scrapData = async (req, res, next) => {
     try {
         let page = await configureTheBrowser();
         let results = await checkDetails(page);
+        await ScrappedData.insertMany(results);
         res.send(results);
     } catch (error) {
         return next(new AppError(500, "failed", "server error"));
--- models/data.js
+++ models/data.js
@@ -1,4 +1,21 @@
 const mongoose = require("mongoose"),
-    dataSchema = new mongoose.Schema({});
+    dataSchema = new mongoose.Schema({
+        name: {
+            type: String,
+            require: true,
+        },
+        price: {
+            type: String,
+            require: true,
+        },
+        link: {
+            type: String,
+            require: true,
+        },
+        img_url: {
+            type: String,
+            require: true,
+        },
+    });
 
 module.exports = mongoose.model("Data", dataSchema);
